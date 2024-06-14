88560dbc205afdab68ce5301d73610cb722f6fb2
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-06-14 16:53:17 +0530

    Update script.js

--- dynamic_content/script.js
+++ dynamic_content/script.js
@@ -1,29 +1,29 @@
-function create_new_element(message) {
-    // Step 1: Create the 'div' element with the class 'left'
-    let newDiv = document.createElement("div");
-    newDiv.className ="left";
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const calcBtn = document.getElementById("calculateBtn");
 
-    // Step 2: Create the 'p' element
-    let newParagraph = document.createElement("p");
-    newParagraph.innerHTML = message;
-
-    // Step 3: Append the 'p' element to the 'div'
-    newDiv.appendChild(newParagraph);
-
-    // Step 4: Append the 'div' to the body
-    document.body.appendChild(newDiv);
+// BMI calclulator function
+function bmiCalculator(weight, height) {
+return (weight / Math.pow(height, 2));
 }
 
-document.getElementById("user-input-form").addEventListener("submit", function(event){
-    event.preventDefault(); // Prevent the form from submitting the traditional way (which would be to reload the page)
+// creates a new paragraph to display the result
+function createPara(bmi) {
+    const newPara = document.createElement("p");
+    newPara.id = "result";
+    newPara.innerHTML = "BMI: " + bmi;
+    document.getElementById('input_box').appendChild(newPara);
+}
 
-    const userInput = document.getElementById("user-input").value;
+calcBtn.addEventListener('click', () => {
+    //read the values from the input fields
+    const weight = parseFloat(weightInput.value);
+    const height = parseFloat(heightInput.value);
 
-    if (userInput.trim().toLowerCase() == "yes") {
-        create_new_element(`Sure, here is the current Date and Time:<br><span>${new Date()}</span>`);
-    } else if (userInput.trim().toLowerCase() == "no"){
-        create_new_element("Have a nice day!")
-    } else {
-        create_new_element("Sorry, wrong input.")
-    }
+    // event to call the bmiCalculator function
+    const rawBMI = bmiCalculator(weight, height);
+    const bmi = Math.round(rawBMI);
+
+    // event to call the createPara function
+    createPara(bmi);
 });
