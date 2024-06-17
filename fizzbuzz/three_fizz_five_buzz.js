e537ec98e2a950806eda14c6040378a42225a98e
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-06-17 14:55:33 +0530

    Update three_fizz_five_buzz.js

--- fizzbuzz/three_fizz_five_buzz.js
+++ fizzbuzz/three_fizz_five_buzz.js
@@ -2,15 +2,20 @@ var output = [];
 var i = 1;
 
 function fizzbuzz() {
-  if (i % 3 === 0 && i % 5 === 0) {
-    output.push("fizzbuzz");
-  } else if (i % 3 === 0) {
-      output.push("fizz");
-  } else if (i % 5 === 0) {
-      output.push("buzz");
+
+  while (i <= 100) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      output.push("fizzbuzz");
+    } else if (i % 3 === 0) {
+        output.push("fizz");
+    } else if (i % 5 === 0) {
+        output.push("buzz");
     } else {
-      output.push(i);
+        output.push(i);
     }
-  i++;
-  console.log(output);
+    i++;
+  }
+  return output;
 }
+
+fizzbuzz();
