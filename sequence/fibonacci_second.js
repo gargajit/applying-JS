34fd6a376b445c2ca677854df3ea7e170650b3e1
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2024-07-14 19:34:36 +0530

    Create fibonacci_second.js

---
+++ sequence/fibonacci_second.js
@@ -0,0 +1,22 @@
+function fibonacciGenerator(n) {
+    if (n <= 0) {
+        console.log("Wrong Input");
+        return;
+    }
+    var fib_list = [];
+
+    if (n === 1) {
+        fib_list.push(0);
+    }
+    else if (n === 2) {
+        fib_list.push(0, 1);
+    }
+    else {
+        fib_list = [0, 1];
+        for (var i = 2; i < n; i++) {
+            fib_list.push(fib_list[i-1] + fib_list[i-2]);
+        }
+    }
+    
+    console.log(fib_list);
+}
