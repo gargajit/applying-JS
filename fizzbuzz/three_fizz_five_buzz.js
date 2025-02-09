5c69aedd6a30f87a170170379d2d34e3a9f2a6fa
Ajit Garg <118595104+gargajit@users.noreply.github.com>
2025-02-09 23:55:42 +0530

    Update three_fizz_five_buzz.js

--- fizzbuzz/three_fizz_five_buzz.js
+++ fizzbuzz/three_fizz_five_buzz.js
@@ -1,6 +1,10 @@
 var output = [];
 var i = 1;
 
+// functioon that returns the array from 1 - 100
+// if the number is divisble only by 3 it will add the string fizz in the array
+// if the number is divisible only by 5 it will add the string buzz in the array
+// if the number is divisble by both 3 and 5 then the string fizzbuzz will be added to the string
 function fizzbuzz() {
 
   while (i <= 100) {
